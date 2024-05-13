8fff7f200a630b898d8c0a86aec8bde855bdd5c6
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2024-05-13 07:57:13 +0200

    add vercel

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,4 +1,5 @@
-import { useRouteError } from 'react-router-dom';
+import Button from '@/components/button';
+import { Link, useRouteError } from 'react-router-dom';
 
 function ErrorPage() {
   const error = useRouteError();
@@ -6,8 +7,11 @@ function ErrorPage() {
   return (
     <div id='error-page' className='border border-black px-2 py-6'>
       <h1 className='text-2xl pb-1'>Oops!</h1>
-      <p className='text-base pb-4'>Sorry, an unexpected error has occurred.</p>
-      <p className='text-base'>{error instanceof Error ? error.message : 'Unexpected error'}</p>
+      <p className='text-base pb-2'>Sorry, an unexpected error has occurred.</p>
+      <p className='text-base pb-4'>{error instanceof Error ? error.message : 'Unexpected error'}</p>
+      <Button variant='primary' asChild className='inline-flex gap-x-2'>
+        <Link to='/'>Go back</Link>
+      </Button>
     </div>
   );
 }

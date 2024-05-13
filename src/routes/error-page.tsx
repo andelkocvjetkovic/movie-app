b5a2ee9523026a68f193919c10885c95c1a0cb0f
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2024-05-13 07:59:41 +0200

    update error-page

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -5,14 +5,16 @@ function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div id='error-page' className='border border-black px-2 py-6'>
-      <h1 className='text-2xl pb-1'>Oops!</h1>
-      <p className='text-base pb-2'>Sorry, an unexpected error has occurred.</p>
-      <p className='text-base pb-4'>{error instanceof Error ? error.message : 'Unexpected error'}</p>
-      <Button variant='primary' asChild className='inline-flex gap-x-2'>
-        <Link to='/'>Go back</Link>
-      </Button>
-    </div>
+    <main className='h-screen flex items-center justify-center'>
+      <div className='border border-black px-6 py-6'>
+        <h1 className='text-2xl pb-1'>Oops! Something went wrong.</h1>
+        <p className='text-base pb-1'>An unexpected error has occurred.</p>
+        <p className='text-base pb-4'>{error instanceof Error ? error.message : 'Unexpected error'}</p>
+        <Button variant='primary' asChild className='inline-flex gap-x-2'>
+          <Link to='/'>Go back</Link>
+        </Button>
+      </div>
+    </main>
   );
 }
 
